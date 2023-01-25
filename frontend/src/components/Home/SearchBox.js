33c2ed947be39579c2dc90528bfe0119303fe213
Jordan <jordan@chude.co>
2023-01-24 20:21:58 -0500

    useEffect comments in SearchBox.js

--- frontend/src/components/Home/SearchBox.js
+++ frontend/src/components/Home/SearchBox.js
@@ -10,8 +10,10 @@ const SearchBox = () => {
     return (
         <div>
             <input type="text" class="form-control" placeholder="What is it that you truly desire?" aria-label="search-box" aria-describedby="basic-addon1" value={searchValue} onChange={handleSearchChange}/>
+            {/* notes */}
             {/* when user inputs text, search database for partial matches*/}
             {/* when partial matches are found, show items with partial matches for every character change */}
+            {/* useEffect for a database search whenever searchValue is changed? */}
         </div>
     );
 };
