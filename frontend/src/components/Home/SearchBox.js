919e2e63bbfe1129ca056f4d3cb71efe68313ded
Jordan <jordan@chude.co>
2023-01-24 19:59:10 -0500

    state handler for searchbox text

--- frontend/src/components/Home/SearchBox.js
+++ frontend/src/components/Home/SearchBox.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SearchBox = () => {
+    const [searchValue, setSearchValue] = useState('');
+    
+    const handleSearchChange = (event) => {
+        setSearchValue(event.target.value);
+    }
+
     return (
         <div>
-            <input type="text" class="form-control" placeholder="What is it that you truly desire?" aria-label="search-box" aria-describedby="basic-addon1"/>
+            <input type="text" class="form-control" placeholder="What is it that you truly desire?" aria-label="search-box" aria-describedby="basic-addon1" value={searchValue} onChange={handleSearchChange}/>
             {/* when user inputs text, search database for partial matches*/}
             {/* when partial matches are found, show items with partial matches for every character change */}
         </div>
