00355dfc4600f61400c7f7537f86f700c659ab00
Jordan <jordan@chude.co>
2023-01-24 19:47:40 -0500

    psuedocode in search box

--- frontend/src/components/Home/SearchBox.js
+++ frontend/src/components/Home/SearchBox.js
@@ -4,6 +4,8 @@ const SearchBox = () => {
     return (
         <div>
             <input type="text" class="form-control" placeholder="What is it that you truly desire?" aria-label="search-box" aria-describedby="basic-addon1"/>
+            {/* when user inputs text, search database for partial matches*/}
+            {/* when partial matches are found, show items with partial matches for every character change */}
         </div>
     );
 };
