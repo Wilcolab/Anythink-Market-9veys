8f997528b1ff242096a427aa2280e30ea99e3ab7
Jordan <jordan@chude.co>
2023-01-25 22:46:11 -0500

    axios get function in SearchBox.js

--- frontend/src/components/Home/SearchBox.js
+++ frontend/src/components/Home/SearchBox.js
@@ -1,19 +1,28 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 const SearchBox = () => {
     const [searchValue, setSearchValue] = useState('');
+    const [suggestions, setSuggestions] = useState([]);
     
     const handleSearchChange = (event) => {
         setSearchValue(event.target.value);
+        if (searchValue.length >= 3) {
+            getData();
+        }
     }
+    
 
+    const getData = async () => {
+        await axios.get(`http://localhost:3000/api/items?title=${searchValue}`)
+        .then(res => {
+            const titles = res.data;
+            setSuggestions(titles);
+        })
+    }
     return (
         <div>
-            <input type="text" class="form-control" placeholder="What is it that you truly desire?" aria-label="search-box" aria-describedby="basic-addon1" value={searchValue} onChange={handleSearchChange}/>
-            {/* notes */}
-            {/* when user inputs text, search database for partial matches*/}
-            {/* when partial matches are found, show items with partial matches for every character change */}
-            {/* useEffect for a database search whenever searchValue is changed? */}
+            <input type="text" class="form-control" placeholder="What is it that you truly desire?" aria-label="search-box" aria-describedby="basic-addon1" value={searchValue} result={suggestions} onChange={handleSearchChange}/>
         </div>
     );
 };
