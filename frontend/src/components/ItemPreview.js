bcb1d3af4d1b8e0e22fe829bb66cdae6bf7757fb
Jordan <jordan@chude.co>
2023-03-27 21:20:20 -0400

    Add "TOP SELLER" label and "verified seller" icon to ItemPreview component

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -53,6 +53,7 @@ const ItemPreview = (props) => {
               alt={item.seller.username}
               className="user-pic rounded-circle pr-1"
             />
+            <img src={process.env.PUBLIC_URL + "/images/verified_seller.svg"} alt="verified-seller" /> TOP SELLER
           </Link>
           <button className="btn btn-outline-secondary" onClick={handleClick}>
             <i className="ion-heart"></i> {item.favoritesCount}
