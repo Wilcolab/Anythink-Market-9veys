21e8f6090913ea0a25ad91ed39ffdf56f7b47459
Jordan <jordan@chude.co>
2023-03-27 21:11:44 -0400

    Add loading state and "no items" message to ItemList component

--- frontend/src/components/ItemList.js
+++ frontend/src/components/ItemList.js
@@ -3,6 +3,7 @@ import ListPagination from "./ListPagination";
 import React from "react";
 
 const ItemList = (props) => {
+
   if (!props.items) {
     return <div className="py-4">Loading...</div>;
   }
@@ -11,7 +12,7 @@ const ItemList = (props) => {
     return <div className="py-4 no-items">No items are here... yet.</div>;
   }
 
-  return (  
+  return (
     <div className="container py-2">
       <div className="row">
         {props.items.map(item => {
